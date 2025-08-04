3a06d4d51234d32436392edb9b21225cd47adffa
SivaBagavathiannanAtNRE <168474917+SivaBagavathiannanAtNRE@users.noreply.github.com>
2025-08-04 14:31:42 -0700

    redid ignoring env.local file and fixed docker-compose.yml to use env_file

--- Dockerfile
+++ Dockerfile
@@ -11,14 +11,22 @@ RUN pip install \
     dagster-aws \
     dagster-docker
 
+
+
+
 # Set $DAGSTER_HOME and copy dagster instance and workspace YAML there
 ENV DAGSTER_HOME=/opt/dagster/dagster_home/
 
 RUN mkdir -p $DAGSTER_HOME
 
 
+
 COPY dagster.yaml workspace.yaml $DAGSTER_HOME
 
 WORKDIR $DAGSTER_HOME
+
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
 
 
--- pipeline_x/job/assets.py
+++ pipeline_x/job/assets.py
@@ -21,11 +21,11 @@ def get_close_api_key():
     logger.info("This is an info message from Dagster logger.")
 
     session = boto3.Session(
-        aws_access_key_id=os.getenv('aws_access_key_id'),
-        aws_secret_access_key=os.getenv('aws_secret_access_key'),
-        region_name=os.getenv('aws_region') 
+        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+        region_name=os.getenv('AWS_REGION') 
     )
-
+    
     client = session.client(
         service_name='secretsmanager',
         region_name=region_name
